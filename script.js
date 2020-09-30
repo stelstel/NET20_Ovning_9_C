6d8c68f269706912948a0d18bba6817ba05d36d0
stelstel <sthlm_32@hotmail.com>
2020-09-30 21:22:00 +0200

    Add task to unordered list

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
 let tasks = [];
 
+// Add task to unordered list
+function addTaskToUList(tasks) {
+    var ul = document.getElementById("taskList");
+    ul.innerHTML = "";
+
+    tasks.forEach((task) => {
+        var li = document.createElement("li");
+        li.appendChild(document.createTextNode(task));
+        ul.appendChild(li);
+    });
+}
+
+// Get input from HTML text box
 function getInput() {
     let inputTask = document.getElementById("task").value;
     document.getElementById("task").value = "";
     tasks.push(inputTask);
-    console.log("T: " + tasks);
+    addTaskToUList(tasks);
 }
 
 document.getElementById("btnAdd").addEventListener("click", getInput);
