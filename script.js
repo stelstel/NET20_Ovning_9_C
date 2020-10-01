d5d2affae8732b7c156a967510728564db8f4be9
stelstel <sthlm_32@hotmail.com>
2020-10-01 22:16:18 +0200

    bought verkar funka

--- script.js
+++ script.js
@@ -1,62 +1,99 @@
+/*
+ul -> 
+      li ->
+            p
+            img
+*/
 let tasks = [];
 
 // Add task to unordered list *********************************
 function addTaskToUList(tasks) {
     var ul = document.getElementById("taskList");
     ul.innerHTML = "";
-
     let counter = 0;
 
     tasks.forEach((task) => {
+        // Create li
         let li = document.createElement("li");
 
+        // Create p
         let liParagraph = document.createElement("p");
-        liParagraph.innerHTML = task;
+
+        // Put text from array in p
+        liParagraph.innerHTML = task.text;
+
+        // Put p in li
         li.appendChild(liParagraph);
 
+        // Give li an id
         li.id = "li-" + counter;
 
+        // Create image
         let imgTrash = document.createElement("img");
         imgTrash.src = "images/trash.png";
-        imgTrash.id = "image-" + counter;
+
+        // Add event listener to image
         imgTrash.addEventListener("click", deleteItem);
+
+        // Put image in li
         li.appendChild(imgTrash);
+
+        if (task.bought == true) {
+            li.classList.add("bought");
+        } else {
+        }
+
+        // Put li in ul
         ul.appendChild(li);
+
+        // Add event listener to p
         let liText = document.querySelector("li#li-" + counter + " p");
-        liText.addEventListener("click", listItemClicked);
+        liText.addEventListener("click", onListItemClick);
+
         counter++;
     });
+    console.table(tasks); /////////////////////////
 }
 
 // Get input from HTML text box *******************************
 function getInput() {
-    let inputTask = document.getElementById("task").value;
+    let inputTaskTxt = document.getElementById("task").value;
 
     //Reset HTML text box
     document.getElementById("task").value = "";
+    let taskObject = {
+        text: inputTaskTxt,
+        bought: false,
+    };
 
-    tasks.push(inputTask);
+    tasks.push(taskObject);
     addTaskToUList(tasks);
 }
 
-function listItemClicked() {
+// User clicked on list item text, change item text look
+function onListItemClick() {
+    let ListItemId = this.parentElement.id;
+    ListItemIndex = ListItemId.substring(3);
+
     if (!this.classList.contains("bought")) {
         this.classList.add("bought");
+        tasks[ListItemIndex].bought = true;
     } else {
         this.classList.remove("bought");
+        tasks[ListItemIndex].bought = false;
     }
 }
 
+// User clicked on list item image, delete
 function deleteItem() {
     this.parentElement.remove(); // Remove from DOM
-
     paragraphInCaller = this.parentElement.querySelector("p").innerHTML;
 
     // Remove from array
     for (let i = 0; i < tasks.length; i++) {
-        const element = tasks[i];
+        const paragraphInArray = tasks[i].text;
 
-        if (element === paragraphInCaller) {
+        if (paragraphInArray === paragraphInCaller) {
             tasks.splice(i, 1);
         }
     }
